a30d2d7fac4bd21d01cb78c7e89fe82535674321
Qmm <1259598502@qq.com>
2025-04-13 08:45:31 +0800

    exp3 task1 shell

--- exp3/shell.c
+++ exp3/shell.c
@@ -1,20 +1,15 @@
-//  shell.c   gcc -o shell ../shell.c
-
+// shell.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-void foo()
-{
-    char * name[2];
-    name[0] = "/bin/sh";
-    name[1] = NULL;
-    execve( name[0], name, NULL);
+void foo() {
+  char *name[3];
+  name[0] = "/bin/cat";
+  name[1] = "//etc/passwd";
+  name[2] = NULL;
+  execve(name[0], name, NULL);
 }
-
-int main(int argc, char * argv[])
-{
-    foo();
-    return 0;
+int main(int argc, char *argv[]) {
+  foo();
+  return 0;
 }
-
